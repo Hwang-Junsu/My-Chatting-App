18f0cdcbdfefe578126d97580872ace2deb6dd6f
Hwang-Junsu <hjs77728@gmail.com>
2023-01-12 18:50:17 +0900

    Fix: User Edit Form Refactor

--- renderer/components/user/userEditModal.tsx
+++ renderer/components/user/userEditModal.tsx
@@ -1,25 +1,25 @@
-import { useRouter } from "next/router";
-import React, { useState } from "react";
-import { IModalProps } from "../../types/modal";
-import Button from "../common/button";
 import { ipcRenderer } from "electron";
 import { onAuthStateChanged } from "firebase/auth";
+import { IModalProps } from "types/modal";
+import Button from "../common/button";
 import { auth, db } from "../../firebase";
 import { doc, updateDoc } from "firebase/firestore";
 import Modal from "../common/modal";
 import Input from "../common/input";
 
 export default function UserEditModal({ isOpen, setIsOpen }: IModalProps) {
-  const [nickName, setNickname] = useState<string>("");
-  const [stateMessage, setStateMessage] = useState<string>("");
-
-  const handleEdit = async (e: React.ChangeEvent<HTMLFormElement>) => {
+  const handleEdit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
+    const displayNameRegex = /.{1,8}/g;
+    const stateMessageRegex = /.{,20}/g;
+
+    const displayName = e.target[0].value;
+    const stateMessage = e.target[1].value;
+
     if (
-      nickName.length > 7 ||
-      nickName.length < 1 ||
-      stateMessage.length > 20
+      !displayNameRegex.test(displayName) ||
+      !stateMessageRegex.test(stateMessage)
     ) {
       ipcRenderer.invoke(
         "showError",
@@ -32,7 +32,7 @@ export default function UserEditModal({ isOpen, setIsOpen }: IModalProps) {
       const userRef = doc(db, "users", user.email);
       await updateDoc(userRef, {
         stateMessage,
-        displayName: nickName,
+        displayName,
       });
     });
 
@@ -45,18 +45,8 @@ export default function UserEditModal({ isOpen, setIsOpen }: IModalProps) {
       <div className="space-y-2">
         <div className="text-lg font-bold text-center">프로필 수정하기</div>
         <form className="flex flex-col space-y-3" onSubmit={handleEdit}>
-          <Input
-            type="text"
-            placeholder="닉네임(8자 이내)."
-            value={nickName}
-            onChange={(e) => setNickname(e.target.value)}
-          />
-          <Input
-            type="text"
-            placeholder="상태 메시지 설정(20자 이내)."
-            value={stateMessage}
-            onChange={(e) => setStateMessage(e.target.value)}
-          />
+          <Input type="text" placeholder="닉네임(8자 이내)." />
+          <Input type="text" placeholder="상태 메시지 설정(20자 이내)." />
           <Button type="submit" text="수정하기" />
         </form>
       </div>
